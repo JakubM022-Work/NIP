47e21b8a729ddef0b006f8c9e4ffe998015213e0
Jakub <jakub.marciniak@inpay.pl>
2024-10-24 17:28:40 +0200

    dsadsad

---
+++ tests/pom_banktransfer_with_entity.spec.js
@@ -0,0 +1,82 @@
+import { test } from '@playwright/test';
+import { CreateTransactionPage } from '../../pageObjects/create-transaction.page';
+import { TRANSACTION } from '../../testingData/entityData';
+import { BASE_URL } from '../../constants';
+
+const baseUrl = BASE_URL;
+
+async function setupPage(browser) {
+  const context = await browser.newContext({
+    storageState: "./auth.json"
+  });
+  const page = await context.newPage();
+  const createTransactionPage = new CreateTransactionPage(page);
+  await createTransactionPage.navigateToCreateTransactionPage(baseUrl);
+  return createTransactionPage;
+}
+
+test.describe('Transaction Creation Tests With Entity Payment Method Bank Transfer', () => {
+  test('creating purchase transaction positive', async ({ browser }) => {
+    const createTransactionPage = await setupPage(browser);
+    await createTransactionPage.selectBuyer();
+    await createTransactionPage.fillTransactionDetailsBank(TRANSACTION);
+    await createTransactionPage.fillBuyerInformationBankEntity(TRANSACTION);
+    await createTransactionPage.fillTitleAndDescription(TRANSACTION);
+    await createTransactionPage.goToDetailsPage();
+  });
+
+  test('creating sale transaction positive', async ({ browser }) => {
+    const createTransactionPage = await setupPage(browser);
+    await createTransactionPage.selectSeller();
+    await createTransactionPage.fillTransactionDetailsBank(TRANSACTION);
+    await createTransactionPage.fillBuyerInformationBankEntity(TRANSACTION);
+    await createTransactionPage.fillTitleAndDescription(TRANSACTION);
+    await createTransactionPage.goToDetailsPage();
+  });
+
+  test('creating transfer transaction positive', async ({ browser }) => {
+    const createTransactionPage = await setupPage(browser);
+    await createTransactionPage.selectTransfer();
+    await createTransactionPage.fillTransactionDetailsTransferBank(TRANSACTION);
+    await createTransactionPage.fillBuyerInformationBankEntity(TRANSACTION, { skipOptionalFields: true });
+    await createTransactionPage.fillSellerInformationEntity(TRANSACTION);
+    await createTransactionPage.fillTitleAndDescription(TRANSACTION);
+    await createTransactionPage.goToDetailsPage();
+  });
+
+  test('creating buyer crypto transaction positive', async ({ browser }) => {
+    const createTransactionPage = await setupPage(browser);
+    await createTransactionPage.selectBuyerCrypto();
+    await createTransactionPage.fillTransactionDetailsCryptoBank(TRANSACTION);
+    await createTransactionPage.fillBuyerInformationBankEntity(TRANSACTION, { skipOptionalFields: true });
+    await createTransactionPage.fillTitleAndDescription(TRANSACTION);
+    await createTransactionPage.goToDetailsPage();
+  });
+
+  test('creating seller crypto transaction positive', async ({ browser }) => {
+    const createTransactionPage = await setupPage(browser);
+    await createTransactionPage.selectSellerCrypto();
+    await createTransactionPage.fillTransactionDetailsCryptoBank(TRANSACTION);
+    await createTransactionPage.fillBuyerInformationBankEntity(TRANSACTION, { skipOptionalFields: true });
+    await createTransactionPage.fillTitleAndDescription(TRANSACTION);
+    await createTransactionPage.goToDetailsPage();
+  });
+
+  test('creating exchange fiat transaction positive', async ({ browser }) => {
+    const createTransactionPage = await setupPage(browser);
+    await createTransactionPage.selectExchangeFiat();
+    await createTransactionPage.fillTransactionDetailsCryptoBank(TRANSACTION, { skipOptionalFields: true });
+    await createTransactionPage.fillBuyerInformationBankEntity(TRANSACTION, { skipOptionalFields: true });
+    await createTransactionPage.fillTitleAndDescription(TRANSACTION);
+    await createTransactionPage.goToDetailsPage();
+  });
+
+  test('creating other transaction positive', async ({ browser }) => {
+    const createTransactionPage = await setupPage(browser);
+    await createTransactionPage.selectOther();
+    await createTransactionPage.fillTransactionDetailsBank(TRANSACTION);
+    await createTransactionPage.fillBuyerInformationOtherEntity(TRANSACTION);
+    await createTransactionPage.fillTitleAndDescription(TRANSACTION);
+    await createTransactionPage.goToDetailsPage();
+  });
+});
---
+++ tests/pom_cash_with_entity.spec.js
@@ -0,0 +1,82 @@
+import { test } from '@playwright/test';
+import { CreateTransactionPage } from '../../pageObjects/create-transaction.page';
+import { TRANSACTION } from '../../testingData/entityData';
+import { BASE_URL } from '../../constants';
+
+const baseUrl = BASE_URL;
+
+async function setupPage(browser) {
+  const context = await browser.newContext({
+    storageState: "./auth.json",
+  });
+  const page = await context.newPage();
+  const createTransactionPage = new CreateTransactionPage(page);
+  await createTransactionPage.navigateToCreateTransactionPage(baseUrl);
+  return createTransactionPage;
+}
+
+test.describe("Transaction Creation Tests With Entity Payment Method Cash", () => {
+  test('creating purchase transaction positive', async ({ browser }) => {
+    const createTransactionPage = await setupPage(browser);
+    await createTransactionPage.selectBuyer();
+    await createTransactionPage.fillTransactionDetailsCash(TRANSACTION);
+    await createTransactionPage.fillBuyerInformationEntity();
+    await createTransactionPage.fillTitleAndDescription(TRANSACTION);
+    await createTransactionPage.goToDetailsPage();
+  });
+
+  test("creating sale transaction positive", async ({ browser }) => {
+    const createTransactionPage = await setupPage(browser);
+    await createTransactionPage.selectSeller();
+    await createTransactionPage.fillTransactionDetailsCash(TRANSACTION);
+    await createTransactionPage.fillBuyerInformationEntity();
+    await createTransactionPage.fillTitleAndDescription(TRANSACTION);
+    await createTransactionPage.goToDetailsPage();
+  });
+
+  test("creating transfer transaction positive", async ({ browser }) => {
+    const createTransactionPage = await setupPage(browser);
+    await createTransactionPage.selectTransfer();
+    await createTransactionPage.fillTransactionDetailsTransferCash(TRANSACTION);
+    await createTransactionPage.fillBuyerInformationEntity({ skipOptionalFields: true });
+    await createTransactionPage.fillSellerInformationEntity(TRANSACTION, { skipOptionalFields: true });
+    await createTransactionPage.fillTitleAndDescription(TRANSACTION);
+    await createTransactionPage.goToDetailsPage();
+  });
+
+  test("creating buyer crypto transaction positive", async ({ browser }) => {
+    const createTransactionPage = await setupPage(browser);
+    await createTransactionPage.selectBuyerCrypto();
+    await createTransactionPage.fillTransactionDetailsCryptoCash(TRANSACTION);
+    await createTransactionPage.fillBuyerInformationEntity({ skipOptionalFields: true });
+    await createTransactionPage.fillTitleAndDescription(TRANSACTION);
+    await createTransactionPage.goToDetailsPage();
+  });
+
+  test("creating seller crypto transaction positive", async ({ browser }) => {
+    const createTransactionPage = await setupPage(browser);
+    await createTransactionPage.selectSellerCrypto();
+    await createTransactionPage.fillTransactionDetailsCryptoCash(TRANSACTION);
+    await createTransactionPage.fillBuyerInformationEntity({ skipOptionalFields: true });
+    await createTransactionPage.fillTitleAndDescription(TRANSACTION);
+    await createTransactionPage.goToDetailsPage();
+  });
+
+  test("creating exchange fiat transaction positive", async ({ browser }) => {
+    const createTransactionPage = await setupPage(browser);
+    await createTransactionPage.selectExchangeFiat();
+    await createTransactionPage.fillTransactionDetailsCryptoCash(TRANSACTION, { skipOptionalFields: true });
+    await createTransactionPage.fillBuyerInformationEntity({ skipOptionalFields: true });
+    await createTransactionPage.fillTitleAndDescription(TRANSACTION);
+    await createTransactionPage.goToDetailsPage();
+  });
+
+  test("creating other transaction positive", async ({ browser }) => {
+    const createTransactionPage = await setupPage(browser);
+    await createTransactionPage.selectOther();
+    await createTransactionPage.fillTransactionDetailsCash(TRANSACTION);
+    await createTransactionPage.fillBuyerInformationOtherEntity(TRANSACTION, { skipOptionalFields: true });
+    await createTransactionPage.fillTitleAndDescription(TRANSACTION);
+    await createTransactionPage.goToDetailsPage();
+  });
+});
